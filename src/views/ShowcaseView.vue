6e06e6fee28858b488cd47f6a406dd80c6dffecc
Muyun <wangzehuan2017@sina.com>
2023-03-11 00:10:26 +0000

    Showcase Page init

--- src/views/ShowcaseView.vue
+++ src/views/ShowcaseView.vue
@@ -1,20 +1,19 @@
 <template>
   <div class="Showcase">
     <nav-bar class="nav"></nav-bar>
-    <el-scrollbar>
-        <div v-for="i in 5" :key="i" class="case">
-          {{ i }}
-        </div>
-    </el-scrollbar>
+    <showcase-cascade></showcase-cascade>
   </div>
 </template>
 
 <script>
-import NavBar from "@/components/NavBar";
+import NavBar from '@/components/NavBar';
+import ShowcaseCascade from '@/components/Showcase/ShowcaseCascade';
+
 export default {
   name: 'ShowcaseView',
   components: {
-    NavBar
+    ShowcaseCascade,
+    NavBar,
   },
 };
 </script>
@@ -29,18 +28,5 @@ export default {
 .nav {
   border-bottom: 0.5px solid rgba(255, 255, 255, 0.3);
   background-color: black;
-}
-
-.el-scrollbar {
-  height: calc(100vh - 120px);
-}
-
-.case {
-  height: calc(100vh - 120px);
-  scroll-snap-align: center;
-}
-
-::v-deep .el-scrollbar__wrap {
-  scroll-snap-type: y mandatory;
 }
 </style>
